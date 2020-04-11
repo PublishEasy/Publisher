13dacb5835e81b60bf07f482bfcc41db56ba90d4
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2020-04-11 20:41:12 +0100

    Add script to check if eslint rules are up to date, and add it to CI

---
+++ scripts/custom-eslint-rules-are-up-to-date.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+## SHELL BOILERPLATE STARTS HERE. DONT TOUCH ANYTHING INCLUDING THESE COMMENTS, ONLY EDIT THROUGH NODE SCRIPT
+
+## You can update all the files by running 'node scripts/update-shell-script-boilerplate.js'.
+## This will also update the filepaths in case you move any files to different directories
+
+# region boilerplate
+
+set -e
+
+ROOT_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")/..
+cd $ROOT_DIRECTORY
+
+# We want relative paths for docker commands specifically and since we use cd relative paths work
+DIRECTORY=scripts
+ROOT_DIRECTORY=.
+
+
+run_command_in_docker_with_write_access () {
+    docker run --rm --mount "type=bind,source=$(pwd),target=/app,consistency=consistent" publisher-base "$@"
+}
+
+run_command_in_docker_with_colors_ctrl_c_capabilitiies_and_updating_file_system () {
+    docker run --rm -it --mount "type=bind,source=$(pwd),target=/app,consistency=consistent" publisher-base "$@"
+}
+
+run_command_in_docker_with_colors () {
+    docker run --rm -t publisher-base "$@"
+}
+
+run_command_in_docker_with_colors_and_write () {
+    docker run --rm -t --mount "type=bind,source=$(pwd),target=/app,consistency=consistent" publisher-base "$@"
+}
+
+run_command_in_docker () {
+    docker run --rm publisher-base "$@"
+}
+
+run_docker_compose_command () {
+    docker-compose -f config/docker/local-dev/docker-compose.yml "$@"
+}
+
+build_docker_image () {
+    docker build -t publisher-base -f config/docker/local-dev/Dockerfile ./
+}
+# endregion
+
+## SHELL BOILERPLATE STOPS HERE. FEEL FREE TO EDIT ANYTHING BELOW THIS COMMENT
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+test -z "$(git diff-index --name-only HEAD --)" || (
+    echo -e "${RED}Working tree should be clean before running this command${NC}" &&
+    exit 1
+)
+
+./scripts/build-eslint-rules.sh
+echo -e "${GREEN}Files built. Just running prettier to normalize diff${NC}"
+run_command_in_docker_with_write_access ./scripts/pre-configured-commands/prettier.sh --write &> /dev/null
+
+# This seems to update the tree making this work somehow. It's magic but it works where before it would give a false negative finding diffs when none existed
+git status &> /dev/null
+test -z "$(git diff-index --name-only HEAD --)" || (
+    echo -e "${RED}It seems you forgot to build the eslint rules after changing them. They are up to date now though the changes are unstaged in git${NC}" &&
+    exit 1
+)
+echo -e "${GREEN}Our custom eslint rules are up to date with source${NC}"
